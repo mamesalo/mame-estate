b207b938fe327055195c4efc0716f36e20d8cc4f
mamesalo <mamesalo7337@gmail.com>
2023-10-17 23:08:51 +0300

    create mango db connection

--- api/index.js
+++ api/index.js
@@ -1,5 +1,16 @@
 import express from "express";
-const app=express();
-app.listen(3000,()=>{
-    console.log('server i running on port 3000');
-})
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+const app = express();
+app.listen(3000, () => {
+  console.log("server i running on port 3000");
+});
