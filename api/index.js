6077bd29d78caf45735b6ac88a8eadc38e73a6d4
mamesalo <mamesalo7337@gmail.com>
2023-10-18 11:16:24 +0300

    complete sign up

--- api/index.js
+++ api/index.js
@@ -17,5 +17,14 @@ app.use(express.json());
 app.listen(3000, () => {
   console.log("server i running on port 3000");
 });
-app.use("/api/user",userRouter)
-app.use("/api/auth",authRouter)
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internet Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
