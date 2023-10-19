49fb0cc3bc1885d80c0aedc7085fb9186b55a705
mamesalo <mamesalo7337@gmail.com>
2023-10-19 15:27:42 +0300

    add upload file functionality

--- clinet/src/pages/Profile.jsx
+++ clinet/src/pages/Profile.jsx
@@ -1,18 +1,71 @@
 import { useSelector } from "react-redux";
-
+import { useRef, useState, useEffect } from "react";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from 'firebase/storage';
 export default function Profile() {
   const { currentUser } = useSelector((state) => state.user);
+  const fileRef = useRef(null);
+  const [file, setFile] = useState(undefined);
+  useEffect(() => {
+    if (file) {
+      handleFileUpload(file);
+    }
+  }, [file]);
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
-      <form className="flex flex-col">
+      <form className="flex flex-col gap-4">
+        <input
+          type="file"
+          ref={fileRef}
+          hidden
+          accept="image/*"
+          onChange={(e) => setFile(e.target.files[0])}
+        />
         <img
+          onClick={() => fileRef.current.click()}
           className="rounded-full h-24 w-24 object-cover
           cursor-pointer self-center mt-2"
           src={currentUser.avatar}
           alt="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkrIN_phrkQozJkBJi9v7jqOEazcPIad5SYA&usqp=CAU"
         />
+        <input
+          type="text"
+          placeholder="username"
+          className="border p-3 rounded-lg"
+          id="username"
+        />
+        <input
+          type="email"
+          placeholder="email"
+          className="border p-3 rounded-lg"
+          id="email"
+        />
+        <input
+          type="password"
+          placeholder="password"
+          className="border p-3 rounded-lg"
+          id="password"
+        />
+        <button
+          className="bg-slate-700 text-white rounded-lg p-3 uppercase 
+        hover:opacity-95 disabled:opacity-80 "
+        >
+          update
+        </button>
       </form>
+      <div className="flex justify-between mt-5">
+        <span className="text-red-700 cursor-pointer hover:underline font-semibold ">
+          Delete account
+        </span>
+        <span className="text-red-700 cursor-pointer hover:underline font-semibold">
+          Sign Out
+        </span>
+      </div>
     </div>
   );
 }
