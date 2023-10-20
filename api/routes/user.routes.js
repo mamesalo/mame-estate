d5923b3f4d71bb0c9dac132e6f17a002d99942b8
mamesalo <mamesalo7337@gmail.com>
2023-10-20 15:29:08 +0300

    finish shownig list

--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -1,7 +1,6 @@
 import express from 'express';
-import {deleteUser, test, updateUser} from '../controllers/user.controller.js';
+import {deleteUser, test, updateUser, getUserListings} from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js.js';
-// import { verifyToken } from '../utils/verifyUser.js';
 
 
 const router = express.Router();
@@ -9,7 +8,7 @@ const router = express.Router();
 router.get('/test', test);
 router.post('/update/:id',verifyToken,updateUser)
 router.delete('/delete/:id', verifyToken, deleteUser)
-// router.get('/listings/:id', verifyToken, getUserListings)
+router.get('/listings/:id', verifyToken, getUserListings)
 // router.get('/:id', verifyToken, getUser)
 
 export default router;
