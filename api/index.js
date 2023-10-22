434a66f847c635515b00e80ab0e56ac6bb4ac29e
mamesalo <mamesalo7337@gmail.com>
2023-10-22 03:42:47 +0300

    fix2

--- api/index.js
+++ api/index.js
@@ -18,10 +18,7 @@ mongoose
 
   const __dirname = path.resolve();
   const app = express();
-  app.use(express.static(path.join(__dirname, '/client/dist')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-  })
+
 
 app.use(express.json());
 app.use(cookiParser());
@@ -31,6 +28,12 @@ app.listen(3000, () => {
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use('/api/listing', listingRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
+
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internet Server Error";
