201a2dfd67b7e9bdf90ca4a2836a306acf17b327
mamesalo <mamesalo7337@gmail.com>
2023-10-22 05:54:38 +0300

    k

--- api/index.js
+++ api/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import userRouter from './routes/user.route.js';
-import authRouter from './routes/auth.route.js';
-import listingRouter from './routes/listing.route.js';
+// import userRouter from './routes/user.route.js';
+// import authRouter from './routes/auth.route.js';
+// import listingRouter from './routes/listing.route.js';
+
 import cookieParser from 'cookie-parser';
 import path from 'path';
+import router from './routes/allRouters.js';
 dotenv.config();
 
 mongoose
@@ -29,9 +31,14 @@ app.listen(3000, () => {
   console.log('Server is running on port 3000!');
 });
 
-app.use('/api/user', userRouter);
-app.use('/api/auth', authRouter);
-app.use('/api/listing', listingRouter);
+app.use('/api/user', router);
+app.use('/api/auth', router);
+app.use('/api/listing', router);
+
+
+// app.use('/api/user', userRouter);
+// app.use('/api/auth', authRouter);
+// app.use('/api/listing', listingRouter);
 
 
 app.use(express.static(path.join(__dirname, '/client/dist')));
--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import userRouter from './api/routes/user.route.js';
-import authRouter from './api/routes/auth.route.js';
-import listingRouter from './api/routes/listing.route.js';
+// import userRouter from './api/routes/user.route.js';
+// import authRouter from './api/routes/auth.route.js';
+// import listingRouter from './api/routes/listing.route.js';
 import cookieParser from 'cookie-parser';
 import path from 'path';
+import router from './api/routes/allRouters.js';
+
 dotenv.config();
 
 mongoose
@@ -29,9 +31,10 @@ app.listen(3000, () => {
   console.log('Server is running on port 3000!');
 });
 
-app.use('/api/user', userRouter);
-app.use('/api/auth', authRouter);
-app.use('/api/listing', listingRouter);
+app.use('/api/user',router);
+app.use('/api/auth', router);
+app.use('/api/listing',router);
+app.use('/api/auth',router)
 
 
 app.use(express.static(path.join(__dirname, '/client/dist')));
