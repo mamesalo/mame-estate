64728ec63606765067745cd7f27af426b58a65c0
mamesalo <mamesalo7337@gmail.com>
2023-10-22 04:09:36 +0300

    fix7

--- clinet/src/App.jsx
+++ clinet/src/App.jsx
@@ -1,23 +1,25 @@
 // import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./Pages/Home";
+
 import SignIn from "./Pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Profile from "./Pages/Profile";
-import About from "./Pages/About";
+
 import MyHeader from "./components/MyHeader";
 import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from "./pages/CreateListing";
 import UpdateListing from "./pages/UpdateListing";
 import Listing from "./pages/Listing";
 import Search from "./pages/Search";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 export default function App() {
   return (
     <BrowserRouter>
       <MyHeader />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element ={<Home/>}/>
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/properties/:propertyId" element={<Listing />} />
@@ -27,7 +29,7 @@ export default function App() {
           <Route path="/new-properties" element={<CreateListing />} />
           <Route path="/update-properties/:propertyId" element={<UpdateListing/>}/>
         </Route>
-        <Route path="/about" element={<About />} />
+        <Route path="/about" element={<About/>}/>
       </Routes>
     </BrowserRouter>
   );
