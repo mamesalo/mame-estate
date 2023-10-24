1f60d872c252be38e8383f25725f83ea99ba9348
mamesalo <mamesalo7337@gmail.com>
2023-10-24 23:36:55 +0300

    connect to cosmos mongo db

--- api/index.js
+++ api/index.js
@@ -1,25 +1,26 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import userRouter from './routes/user.routes.js';
-import authRouter from './routes/auth.routes.js';
-import listingRouter from './routes/listing.routes.js';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/user.routes.js";
+import authRouter from "./routes/auth.routes.js";
+import listingRouter from "./routes/listing.routes.js";
 
-import cookieParser from 'cookie-parser';
-import path from 'path';
+import cookieParser from "cookie-parser";
+import path from "path";
 
 dotenv.config();
 
+mongoose.Promise = global.Promise;
 mongoose
-  .connect(process.env.MONGO)
+  .connect(process.env.AZURE_MONGO)
   .then(() => {
-    console.log('Connected to MongoDB!');
+    console.log("Connected to Cosmos MongoDB!");
   })
   .catch((err) => {
     console.log(err);
   });
 
-  const __dirname = path.resolve();
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -28,25 +29,22 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000!');
+  console.log("Server is running on port 3000!");
 });
 
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 
+// app.use(express.static(path.join(__dirname, "/clinet/dist")));
 
-app.use('/api/user', userRouter);
-app.use('/api/auth', authRouter);
-app.use('/api/listing', listingRouter);
-
-
-app.use(express.static(path.join(__dirname, '/clinet/dist')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'clinet', 'dist', 'index.html'));
-})
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "clinet", "dist", "index.html"));
+// });
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal Server Error';
+  const message = err.message || "Internal Server Error";
   return res.status(statusCode).json({
     success: false,
     statusCode,
