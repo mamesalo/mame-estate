aa93cb3b206073d7c7fcec50b091d3d55d00430d
mamesalo <117835018+mamesalo@users.noreply.github.com>
2023-10-25 14:39:49 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -36,11 +36,11 @@ app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
 
-// app.use(express.static(path.join(__dirname, "/clinet/dist")));
+ app.use(express.static(path.join(__dirname, "/clinet/dist")));
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "clinet", "dist", "index.html"));
-// });
+ app.get("*", (req, res) => {
+   res.sendFile(path.join(__dirname, "clinet", "dist", "index.html"));
+ });
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
