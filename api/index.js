435e387da0be169170f698de349debef7858ce11
mamesalo <mamesalo7337@gmail.com>
2023-10-28 02:50:38 +0300

    deploye

--- api/index.js
+++ api/index.js
@@ -40,16 +40,16 @@ app.listen(3000, () => {
 
 
 
-app.use('/api/user', userRouter);
-app.use('/api/auth', authRouter);
-app.use('/api/listing', listingRouter);
+// app.use('/api/user', userRouter);
+// app.use('/api/auth', authRouter);
+// app.use('/api/listing', listingRouter);
 
  
-app.use(express.static(path.join(__dirname, '/clinet/dist')));
+// app.use(express.static(path.join(__dirname, '/clinet/dist')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'clinet', 'dist', 'index.html'));
-})
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'clinet', 'dist', 'index.html'));
+// })
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
