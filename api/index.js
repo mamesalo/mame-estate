d41618d226cd6ed0e28c306ec9fe524d74ce8b5b
mamesalo <117835018+mamesalo@users.noreply.github.com>
2023-10-30 23:04:54 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,7 +4,6 @@ import dotenv from "dotenv";
 import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import listingRouter from "./routes/listing.routes.js";
-import Listing from "./models/listing.model.js";
 import cookieParser from "cookie-parser";
 import path from "path";
 
@@ -34,69 +33,7 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
-//app.use("/api/listing", listingRouter);
-
-
-
-//........................................................................
-
-
-  app.get('/api/listing/get', async (req, res) => {
-  try {
-    const limit = parseInt(req.query.limit) || 9;
-    const startIndex = parseInt(req.query.startIndex) || 0;
-    let offer = req.query.offer;
-
-    if (offer === undefined || offer === 'false') {
-      offer = { $in: [false, true] };
-    }
-
-    let furnished = req.query.furnished;
-
-    if (furnished === undefined || furnished === 'false') {
-      furnished = { $in: [false, true] };
-    }
-
-    let parking = req.query.parking;
-
-    if (parking === undefined || parking === 'false') {
-      parking = { $in: [false, true] };
-    }
-
-    let type = req.query.type;
-
-    if (type === undefined || type === 'all') {
-      type = { $in: ['sale', 'rent'] };
-    }
-
-    const searchTerm = req.query.searchTerm || '';
-
-   // const sort = req.query.sort || 'createdAt';
-
-    const order = req.query.order || 'desc';
-
-    const listings = await Listing.find({
-      name: { $regex: searchTerm, $options: 'i' },
-      offer,
-      furnished,
-      parking,
-      type,
-    })
-     // .sort({ [sort]: order })
-      .limit(limit)
-      .skip(startIndex);
-
-    return res.status(200).json(listings);
-  } catch (error) {
-    next(error);
-  }
-});
-
-//..................................................................
-
-
-
-
+app.use("/api/listing", listingRouter);
 
 
  app.use(express.static(path.join(__dirname, "/clinet/dist")));
