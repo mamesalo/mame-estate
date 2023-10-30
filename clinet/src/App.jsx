f8df127fabada6904708f36e88fbc51400db5d09
mamesalo <mamesalo7337@gmail.com>
2023-10-30 14:43:59 +0300

    gg

--- clinet/src/App.jsx
+++ clinet/src/App.jsx
@@ -1,44 +1,36 @@
-// // import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-// import SignUp from "./pages/SignUp";
+import SignUp from "./pages/SignUp";
 
-// import MyHeader from "./components/MyHeader";
-// import PrivateRoute from "./components/PrivateRoute";
-// import CreateListing from "./pages/CreateListing";
-// import UpdateListing from "./pages/UpdateListing";
-// import Listing from "./pages/Listing";
-// import Search from "./pages/Search";
-// import Home from "./pages/Home";
-// import About from "./pages/About";
-// import Profile from "./pages/NewProfile";
-// import NewSignIn from "./pages/NewSignIn";
-
-// export default function App() {
-//   return (
-//     <BrowserRouter>
-//       <MyHeader />
-//       <Routes>
-//         <Route path="/" element ={<Home/>}/>
-//         <Route path="/sign-in" element ={<NewSignIn/>}/>
-//         <Route path="/sign-up" element={<SignUp />} />
-//         <Route path="/properties/:propertyId" element={<Listing />} />
-//         <Route path="/search" element={<Search />} />
-//         <Route element={<PrivateRoute />}>
-//           <Route path="/new-properties" element={<CreateListing />} />
-//           <Route path="/update-properties/:propertyId" element={<UpdateListing/>}/>
-//           <Route path="/profile" element={<Profile/>}/>
-//         </Route>
-//         <Route path="/about" element={<About/>}/>
-//       </Routes>
-//     </BrowserRouter>
-//   );
-// }
-
-import React from 'react'
+import MyHeader from "./components/MyHeader";
+import PrivateRoute from "./components/PrivateRoute";
+import CreateListing from "./pages/CreateListing";
+import UpdateListing from "./pages/UpdateListing";
+import Listing from "./pages/Listing";
+import Search from "./pages/Search";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/NewProfile";
+import NewSignIn from "./pages/NewSignIn";
 
 export default function App() {
   return (
-    <div>My app</div>
-  )
+    <BrowserRouter>
+      <MyHeader />
+      <Routes>
+        <Route path="/" element ={<Home/>}/>
+        <Route path="/sign-in" element ={<NewSignIn/>}/>
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/properties/:propertyId" element={<Listing />} />
+        <Route path="/search" element={<Search />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/new-properties" element={<CreateListing />} />
+          <Route path="/update-properties/:propertyId" element={<UpdateListing/>}/>
+          <Route path="/profile" element={<Profile/>}/>
+        </Route>
+        <Route path="/about" element={<About/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
 }
