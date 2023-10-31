cda0b55dc1c564fea6e77c7f2ca18c1d7ea9c377
mamesalo <mamesalo7337@gmail.com>
2023-10-31 17:19:36 +0300

    done

--- clinet/src/pages/NewProfile.jsx
+++ clinet/src/pages/NewProfile.jsx
@@ -275,7 +275,7 @@ export default function Profile() {
               key={listing._id}
               className="border rounded-lg p-3 flex justify-between items-center gap-4"
             >
-              <Link to={`/listing/${listing._id}`}>
+              <Link to={`/properties/${listing._id}`}>
                 <img
                   src={listing.imageUrls[0]}
                   alt="listing cover"
